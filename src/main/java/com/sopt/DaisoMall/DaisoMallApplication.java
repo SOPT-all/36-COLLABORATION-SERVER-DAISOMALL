92181535a49edd14a7e5d00abe68f45154a739db
huncozyboy <leejh5838@naver.com>
2025-05-11 22:44:05 +0900

    chore: initial commit

---
+++ src/main/java/com/sopt/DaisoMall/DaisoMallApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.DaisoMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisoMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisoMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopt/DaisoMall/DaisoMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.DaisoMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisoMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
