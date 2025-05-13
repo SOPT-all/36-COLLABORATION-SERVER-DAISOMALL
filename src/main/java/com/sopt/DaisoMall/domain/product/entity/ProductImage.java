42935f0a1a4cc334ffdba7c190b5e1d5e0690ef0
huncozyboy <leejh5838@naver.com>
2025-05-14 04:05:35 +0900

    feat : S3 이미지 관련 도메인 추가

---
+++ src/main/java/com/sopt/DaisoMall/domain/product/entity/ProductImage.java
@@ -0,0 +1,34 @@
+package com.sopt.DaisoMall.domain.product.entity;
+
+import com.sopt.DaisoMall.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+public class ProductImage extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private boolean isMain = false;
+
+    @Column(nullable = false)
+    private int sortOrder = 0;
+}
