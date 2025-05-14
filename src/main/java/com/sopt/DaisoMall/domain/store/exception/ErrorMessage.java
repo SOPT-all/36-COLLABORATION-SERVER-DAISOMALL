8a13d5941ec8464731f582e27bd1ea623f61bb68
huncozyboy <leejh5838@naver.com>
2025-05-15 01:47:10 +0900

    feat : 존재하지 않는 페이지 예외처리 추가

--- src/main/java/com/sopt/DaisoMall/domain/store/exception/ErrorMessage.java
+++ src/main/java/com/sopt/DaisoMall/domain/store/exception/ErrorMessage.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorMessage {
+    NOT_FOUND_PAGE("존재하지 않는 페이지입니다"),
     NOT_FOUND_BRAND ("존재하지 않는 브랜드입니다");
 
     private final String message;
