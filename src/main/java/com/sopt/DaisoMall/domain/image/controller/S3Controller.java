aa2a3f5ddf29222fe17503a07038b35aebf59c96
huncozyboy <leejh5838@naver.com>
2025-05-16 13:36:48 +0900

    feat : S3 컨트롤러 형식만 구현

---
+++ src/main/java/com/sopt/DaisoMall/domain/image/controller/S3Controller.java
@@ -0,0 +1,11 @@
+package com.sopt.DaisoMall.domain.image.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/s3")
+public class S3Controller {
+}
