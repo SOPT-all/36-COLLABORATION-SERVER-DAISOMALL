8e2c55770b0f7499f23eca21164aefda68a5f728
huncozyboy <leejh5838@naver.com>
2025-05-16 17:34:40 +0900

    refactor : productCode로 올바른 필드 함께 반환

--- src/main/java/com/sopt/DaisoMall/domain/product/dto/response/ProductResponse.java
+++ src/main/java/com/sopt/DaisoMall/domain/product/dto/response/ProductResponse.java
@@ -5,12 +5,14 @@ import com.sopt.DaisoMall.domain.product.entity.Product;
 public record ProductResponse(
         Long productId,
         String productName,
+        String productCode,
         int price
 ) {
     public static ProductResponse from(Product product) {
         return new ProductResponse(
                 product.getId(),
                 product.getProductName(),
+                product.getProductCode(),
                 product.getPrice()
         );
     }
