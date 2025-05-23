3091c92198669de6e36ce379ca4be3b7a5ea0aa9
huncozyboy <leejh5838@naver.com>
2025-05-23 11:10:59 +0900

    refactor : 검색 리스트 반환시 메인 이미지 함께 반환

--- src/main/java/com/sopt/DaisoMall/domain/product/dto/response/ProductResponse.java
+++ src/main/java/com/sopt/DaisoMall/domain/product/dto/response/ProductResponse.java
@@ -1,19 +1,28 @@
 package com.sopt.DaisoMall.domain.product.dto.response;
 
 import com.sopt.DaisoMall.domain.product.entity.Product;
+import com.sopt.DaisoMall.domain.product.entity.ProductImage;
 
 public record ProductResponse(
         Long productId,
         String productName,
         String productCode,
-        int price
+        int price,
+        String mainImage
 ) {
     public static ProductResponse from(Product product) {
+        String imageUrl = product.getProductImages().stream()
+                .filter(image -> image.isMain() && image.getSortOrder() == 0)
+                .map(ProductImage::getImageUrl)
+                .findFirst()
+                .orElse(null);
+
         return new ProductResponse(
                 product.getId(),
                 product.getProductName(),
                 product.getProductCode(),
-                product.getPrice()
+                product.getPrice(),
+                imageUrl
         );
     }
 }
